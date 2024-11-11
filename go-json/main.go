3aab440c7fa229e5d6ec2bf18a14238de0336cbc
MFarhanFansuri <farhanfansurigithub@gamil.com>
2024-11-11 09:15:02 +0700

    belajar json

--- go-json/main.go
+++ go-json/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"reflect"
 )
 
 type Person struct {
@@ -29,4 +30,46 @@ func main() {
 
 	fmt.Println(john)
 
+	// Array of Object
+
+	var jsonStrings = `[
+	{
+		"name":"Ronaldo",
+		"age":19
+	},
+		{
+		"name":"Messi",
+		"age":21
+	}
+	]`
+
+	var footballers []Person
+	var jsonFootbalers = []byte(jsonStrings)
+	var err2 = json.Unmarshal(jsonFootbalers, &footballers)
+
+	if err2 != nil {
+		fmt.Println(err2.Error())
+	}
+
+	fmt.Println(footballers[0])
+
+	// encode
+	var ToJsonFootbaler = []Person{
+		{
+			Name: "Cristiano",
+			Age:  21,
+		},
+		{
+			Name: "Messi",
+			Age:  21,
+		},
+	}
+	jsonDataFootbaler, err3 := json.Marshal(ToJsonFootbaler)
+	if err3 != nil {
+		fmt.Println(err3.Error())
+		return
+	}
+
+	fmt.Println(string(jsonDataFootbaler))
+	fmt.Println(reflect.TypeOf(jsonDataFootbaler))
 }
