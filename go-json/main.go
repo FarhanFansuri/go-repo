f3adb780931f3306140f1b8ed600b62e6e6daedd
MFarhanFansuri <farhanfansurigithub@gamil.com>
2024-11-11 08:41:38 +0700

    belajar json

--- go-json/main.go
+++ go-json/main.go
@@ -1,5 +1,32 @@
 package main
 
-func main(){
-	
+import (
+	"encoding/json"
+	"fmt"
+)
+
+type Person struct {
+	Name string `json:name`
+	Age  int    `json:age`
+}
+
+func main() {
+	//JSON
+	jsonString := `{
+	"name" : "John",
+	"age":21
+	}`
+
+	var jsonData = []byte(jsonString)
+
+	var john Person
+	var err = json.Unmarshal(jsonData, &john)
+
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	fmt.Println(john)
+
 }
